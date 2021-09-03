478c1e686703831600a73b59d30d593c454679c1
vicky-hemnani <58837151+vicky-hemnani@users.noreply.github.com>
2021-09-03 19:57:10 +0530

    album-imp commited

--- app/src/main/java/com/example/musicplayervi/AlbumDetails.java
+++ app/src/main/java/com/example/musicplayervi/AlbumDetails.java
@@ -1,14 +1,60 @@
 package com.example.musicplayervi;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.example.musicplayervi.MainActivity.musicfiles;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
 
 public class AlbumDetails extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    ImageView albumPic;
+    String albumName;
+    ArrayList<Music> albumSongs=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album_details);
+        recyclerView = findViewById(R.id.recyclerView);
+        albumPic = findViewById(R.id.albumPic);
+        albumName = getIntent().getStringExtra("albumName");
+        int j = 0;
+        for (int i = 0; i < musicfiles.size(); i++)
+        {
+            if(albumName.equals(musicfiles.get(i).getAlbum()));
+            {
+                Log.d("Album Name", "onCreate: "+musicfiles.get(i).getAlbum()+"--------"+albumName);
+                albumSongs.add(j,musicfiles.get(i));
+                j++;
+            }
+        }
+        byte[] image=getalbum(albumSongs.get(1).getPath());
+        Log.d("what", "Waht is error "+albumName);
+        if(image!=null)
+        {
+            Glide.with(this).load(image).into(albumPic);
+        }
+        else
+        {
+            Glide.with(this).load(R.drawable.imgg).into(albumPic);
+        }
+
+    }
+    private byte[] getalbum(String uri)
+    {
+        MediaMetadataRetriever retriever=new MediaMetadataRetriever();
+        retriever.setDataSource(uri);
+        byte[] art=retriever.getEmbeddedPicture();
+        retriever.release();
+        return art;
     }
 }
